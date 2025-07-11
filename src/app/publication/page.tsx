e80f270f505370f1d4ec5d11038cfcf74e712ce0
rzalwhy12 <rzalcorp05@gmail.com>
2025-07-12 02:31:32 +0700

    styky

--- src/app/publication/page.tsx
+++ src/app/publication/page.tsx
@@ -30,11 +30,10 @@ export default function PublicationPage() {
                 <PublicationContent />
             </Suspense>
 
-            {/* Tombol untuk mengakses /publication/dashboardoc */}
-            <div className="fixed bottom-4 left-4 z-50"> {/* Posisi di kiri bawah */}
+
+            <div className="sticky bottom-20 left-8 z-0">
                 <Link href="/publication/dashboardoc">
                     <Button className="shadow-lg bg-blue-600 hover:bg-blue-700 text-white p-4 rounded-full flex items-center justify-center">
-                        {/* Anda bisa tambahkan ikon di sini, contoh: */}
                         <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
                             <path strokeLinecap="round" strokeLinejoin="round" d="M10 20l4-16m4 4l4 4-4 4M6 16l-4-4 4-4" />
                         </svg>
